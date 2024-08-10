a46a8ccd8c9ee5d37f172caadfd679b3deec5ffe
oneangel <angel09reyes29@gmail.com>
2024-08-10 00:08:36 -0600

    register stepper

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,3 @@
-// Register.js
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -15,6 +14,7 @@ const Register = () => {
     fechaDeNacimiento: "",
   });
   const [files, setFiles] = useState({});
+  const [step, setStep] = useState(1);
 
   const handleChange = (e) => {
     setFormData({
@@ -67,6 +67,14 @@ const Register = () => {
     }
   };
 
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
   return (
     <div className="h-screen grid-cols-5 2xl:grid">
       <div className="hidden col-span-2 my-auto mx-36 2xl:block">
@@ -74,118 +82,139 @@ const Register = () => {
           <img src={registerImage} alt="" className="" />
         </div>
       </div>
-      <div className="flex flex-col col-span-3 my-auto bg-white mx-14 lg:mx-56 2xl:-ml-10">
+      <div className="flex flex-col justify-center col-span-3 my-20 bg-white mx-14 lg:mx-56 2xl:-ml-10">
         <h2 className="text-4xl font-semibold">Registrarse</h2>
         <p className="text-lg text-[#313131] mt-4 mb-9 ">
           Vamos a prepararte para que puedas acceder a tu cuenta personal.
         </p>
         <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="grid grid-cols-2 gap-8">
-            <div className="flex flex-col col-span-1 gap-8">
+          {step === 1 && (
+            <div className="grid grid-cols-2 gap-8">
+              <div className="flex flex-col col-span-1 gap-8">
+                <Input
+                  type="text"
+                  id="nombre"
+                  name="nombre"
+                  placeholder="Nombre"
+                  value={formData.nombre}
+                  onChange={handleChange}
+                  required
+                />
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Contraseña"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex flex-col col-span-1 gap-8">
+                <Input
+                  type="text"
+                  id="CURP"
+                  name="CURP"
+                  placeholder="CURP"
+                  value={formData.CURP}
+                  onChange={handleChange}
+                  required
+                />
+                <Input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirmar Contraseña"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
               <Input
-                type="text"
-                id="nombre"
-                name="nombre"
-                placeholder="Nombre"
-                value={formData.nombre}
-                onChange={handleChange}
-                required
-              />
-              <Input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Contraseña"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
+                  type="number"
+                  id="numero"
+                  name="numero"
+                  className="w-full col-span-2"
+                  placeholder="Introduzca su numero"
+                  value={formData.numero}
+                  onChange={handleChange}
+                  required
+                />
             </div>
-            <div className="flex flex-col col-span-1 gap-8">
-              <Input
-                type="text"
-                id="CURP"
-                name="CURP"
-                placeholder="CURP"
-                value={formData.CURP}
-                onChange={handleChange}
-                required
-              />
+            
+          )}
+          {step === 2 && (
+            <>
+              <div>
+                <label htmlFor="fechaDeNacimiento">Fecha de Nacimiento</label>
+                <Input
+                  type="date"
+                  id="fechaDeNacimiento"
+                  name="fechaDeNacimiento"
+                  value={formData.fechaDeNacimiento}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-              <Input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                placeholder="Confirmar Contraseña"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                required
-              />
-              <Input
-                type="number"
-                id="numero"
-                name="numero"
-                placeholder="Introduzca su numero"
-                value={formData.numero}
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div>
+                <label htmlFor="INE">INE</label>
+                <Input
+                  type="file"
+                  id="INE"
+                  name="INE"
+                  onChange={handleFileChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="actaNacimiento">Acta de Nacimiento</label>
+                <Input
+                  type="file"
+                  id="actaNacimiento"
+                  name="actaNacimiento"
+                  onChange={handleFileChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="comprobanteDomicilio">
+                  Comprobante de Domicilio
+                </label>
+                <Input
+                  type="file"
+                  id="comprobanteDomicilio"
+                  name="comprobanteDomicilio"
+                  onChange={handleFileChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="comprobanteIngrsos">
+                  Comprobante de Ingresos
+                </label>
+                <Input
+                  type="file"
+                  id="comprobanteIngrsos"
+                  name="comprobanteIngrsos"
+                  onChange={handleFileChange}
+                />
+              </div>
+            </>
+          )}
+          <div className="flex justify-between">
+            {step > 1 && (
+              <Button type="button" onClick={prevStep} className="w-full mr-2">
+                Anterior
+              </Button>
+            )}
+            {step < 2 ? (
+              <Button type="button" onClick={nextStep} className="w-full">
+                Siguiente
+              </Button>
+            ) : (
+              <Button type="submit" className="w-full">
+                Registrarse
+              </Button>
+            )}
           </div>
-          <div>
-            <label htmlFor="fechaDeNacimiento">Fecha de Nacimiento</label>
-            <Input
-              type="date"
-              id="fechaDeNacimiento"
-              name="fechaDeNacimiento"
-              value={formData.fechaDeNacimiento}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="INE">INE</label>
-            <Input
-              type="file"
-              id="INE"
-              name="INE"
-              onChange={handleFileChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="actaNacimiento">Acta de Nacimiento</label>
-            <Input
-              type="file"
-              id="actaNacimiento"
-              name="actaNacimiento"
-              onChange={handleFileChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="comprobanteDomicilio">
-              Comprobante de Domicilio
-            </label>
-            <Input
-              type="file"
-              id="comprobanteDomicilio"
-              name="comprobanteDomicilio"
-              onChange={handleFileChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="comprobanteIngrsos">Comprobante de Ingresos</label>
-            <Input
-              type="file"
-              id="comprobanteIngrsos"
-              name="comprobanteIngrsos"
-              onChange={handleFileChange}
-            />
-          </div>
-
-          <Button type="submit" className="w-full">
-            Registrarse
-          </Button>
         </form>
       </div>
     </div>
